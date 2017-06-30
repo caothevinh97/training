22c9ba6d18c74715b983cab92a906ec838ad1bed
caothevinh97 <demonaction2505@gmail.com>
2017-06-30 19:28:02 +0700

    Create

--- news.php
+++ news.php
@@ -1,9 +1,29 @@
+<?php
+	$sql = "SELECT * FROM  news";
+	$query = mysql_query($sql);
+	mysql_fetch_assoc($query);
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Cao The Vinh</title>
 </head>
 <body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	<h1>1113</h1>
 	<h1>1113</h1>
 	<h1>1113</h1>
